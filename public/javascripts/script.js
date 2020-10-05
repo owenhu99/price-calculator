33ef240165f064146659e5517a3064367d44d618
Xiao Owen Hu <owen.hu@mail.utoronto.ca>
2020-10-04 23:57:44 -0400

    Implemented tax and discount
    You can now modify tax and discount and they will be reflected next to the total.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,6 @@
 var items = [];
+var discount = 0;
+var tax = 13; // default 13%
 
 const rowDelete = "<td class='text-right'><button type='button' class='close' aria-label='Close'><span aria-hidden='true'>&times;</span></button></td>";
 
@@ -13,7 +15,8 @@ $('#addItem').click(function(){
     items.push(dict);
 
     $('#myTable tr:last').after("<tr><td>"+str+"</td><td>1</td><td>10</td><td>10</td>"+rowDelete+"</tr>");
-    $('#total').text("Total $" + getSum());
+    var sum = getSum();
+    $('#total').html("Discount -$"+ sum[0] +" &emsp; Tax $"+sum[1]+" &emsp; <b>Total $"+sum[2]+"</b>");
 });
 
 $('#myTable').on('click', 'button.close', function(e){
@@ -21,7 +24,8 @@ $('#myTable').on('click', 'button.close', function(e){
     var nRow = $(this).parents('tr')[0];
     $(nRow).remove();
     items.splice(nRow.rowIndex - 1, 1);
-    $('#total').text("Total $" + getSum());
+    var sum = getSum();
+    $('#total').html("Discount -$"+ sum[0] +" &emsp; Tax $"+sum[1]+" &emsp; <b>Total $"+sum[2]+"</b>");
 });
 
 function getSum() {
@@ -29,5 +33,32 @@ function getSum() {
     for (var i = 0; i < items.length; i++) {
         sum += items[i].quantity * items[i].price;
     };
-    return sum.toFixed(2);
+    var discountTotal = sum * (discount / 100);
+    var taxTotal = sum * (tax / 100);
+    if (discount) {
+        sum = sum * (1 + (tax / 100)) * (1 - (discount / 100));
+    } else {
+        sum = sum * (1 + (tax / 100));
+    }
+    return [discountTotal.toFixed(2), taxTotal.toFixed(2), sum.toFixed(2)];
 }
+
+$('#applyTax').click(function(){
+    var newTax = $('#tax').val();
+    if (newTax != "") {
+        tax = newTax;
+    }
+    $('#modalTaxRate').modal('hide');
+    var sum = getSum();
+    $('#total').html("Discount -$"+ sum[0] +" &emsp; Tax $"+sum[1]+" &emsp; <b>Total $"+sum[2]+"</b>");
+});
+
+$('#applyDiscount').click(function(){
+    var newDiscount = $('#discount').val();
+    if (newDiscount != "") {
+        discount = newDiscount;
+    }
+    $('#modalDiscount').modal('hide');
+    var sum = getSum();
+    $('#total').html("Discount -$"+ sum[0] +" &emsp; Tax $"+sum[1]+" &emsp; <b>Total $"+sum[2]+"</b>");
+});

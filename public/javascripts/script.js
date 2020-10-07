9bb0be6f8ecfcee3661c89491ab20db068e92005
Xiao Owen Hu <owen.hu@mail.utoronto.ca>
2020-10-07 18:09:40 -0400

    Cleaned up code
    - Added comments for functions
    - Rearranged code for readability

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,6 +4,7 @@ var tax = 13; // default 13%
 
 const rowDelete = "<td class='text-right'><button type='button' class='close' aria-label='Close'><span aria-hidden='true'>&times;</span></button></td>";
 
+/** Adding item **/
 $('#addItem').click(function(){
     var upc = $('#item').val();
     $('#item').val('');
@@ -23,6 +24,7 @@ $('#addItem').click(function(){
     });
 });
 
+/** Removing an item **/
 $('#myTable').on('click', 'button.close', function(e){
     e.preventDefault();
     var nRow = $(this).parents('tr')[0];
@@ -36,12 +38,41 @@ $('#myTable').on('click', 'button.close', function(e){
         console.log("subtract from table");
         $(nRow).find('.quantity').text(remaining);
     }
-    
-    var sum = getSum();
-    $('#total').html("Discount -$"+ sum[0] +" &emsp; Tax $"+sum[1]+" &emsp; <b>Total $"+sum[2]+"</b>");
+    console.log(items);
+    updateSum();
 });
 
+/** Applying tax rate **/
+$('#applyTax').click(function(){
+    var newTax = $('#tax').val();
+    if (newTax != "") {
+        tax = newTax;
+    }
+    $('#modalTaxRate').modal('hide');
+    updateSum();
+});
+
+/** Applying discount rate **/
+$('#applyDiscount').click(function(){
+    var newDiscount = $('#discount').val();
+    if (newDiscount != "") {
+        discount = newDiscount;
+    }
+    $('#modalDiscount').modal('hide');
+    updateSum();
+});
+
+/** ---------------- **/
+/** HELPER FUNCTIONS **/
+/** ---------------- **/
+
 function deleteItem(id) {
+    /**
+     * Returns:
+     * 0 to remove item from items
+     * >=1 to subtract item quantity by 1
+     * -1 for error (item not found)
+     */
     var upc = parseInt(id.substring(1));
     for (var i = 0; i < items.length; i++) {
         if (items[i].upc == upc) {
@@ -58,6 +89,12 @@ function deleteItem(id) {
 }
 
 function getSum() {
+    /**
+     * Returns [a, b, c]:
+     * a = amount of discount
+     * b = amount of tax
+     * c = total checkout amount
+     */
     var sum = 0;
     for (var i = 0; i < items.length; i++) {
         sum += items[i].quantity * items[i].price;
@@ -72,47 +109,29 @@ function getSum() {
     return [discountTotal.toFixed(2), taxTotal.toFixed(2), sum.toFixed(2)];
 }
 
-$('#applyTax').click(function(){
-    var newTax = $('#tax').val();
-    if (newTax != "") {
-        tax = newTax;
-    }
-    $('#modalTaxRate').modal('hide');
-    var sum = getSum();
-    $('#total').html("Discount -$"+ sum[0] +" &emsp; Tax $"+sum[1]+" &emsp; <b>Total $"+sum[2]+"</b>");
-});
-
-$('#applyDiscount').click(function(){
-    var newDiscount = $('#discount').val();
-    if (newDiscount != "") {
-        discount = newDiscount;
-    }
-    $('#modalDiscount').modal('hide');
-    var sum = getSum();
-    $('#total').html("Discount -$"+ sum[0] +" &emsp; Tax $"+sum[1]+" &emsp; <b>Total $"+sum[2]+"</b>");
-});
-
-function onSuccess (result) {
-    if (!result.found) {
-        alert("Item not found");
-        return;
-    }
-
-    var numToAdd = addItem(result);
-
-    if (numToAdd) {
-        // render updated row
-        $("#u"+ result.upc +"").html(returnItemLine(result, numToAdd));
-    } else {
-        // render new row
-        $('#myTable tr:last').after(returnItemLine(result, 0));
-    }
-
+function updateSum() {
+    /** Render sum to screen */
     var sum = getSum();
     $('#total').html("Discount -$"+ sum[0] +" &emsp; Tax $"+sum[1]+" &emsp; <b>Total $"+sum[2]+"</b>");
 }
 
+function onSuccess (result) {
+    /** To execute synchronously upon AJAX POST request **/
+    if (!result.found) {
+        alert("Item not found");
+        return;
+    }
+    var numToAdd = addItem(result);
+    if (numToAdd) {
+        $("#u"+ result.upc +"").html(returnItemLine(result, numToAdd));
+    } else {
+        $('#myTable tr:last').after(returnItemLine(result, 0));
+    }
+    updateSum();
+}
+
 function returnItemLine (data, numToAdd) {
+    /** Return appropriate html string to update item */
     if (numToAdd) {
         return("<td>"+data.upc+"</td><td>"+data.name+"</td><td class='quantity'>"+numToAdd+"</td><td>"+data.price+"</td><td>"
             +(data.price * numToAdd).toFixed(2)+"</td>"+rowDelete);
@@ -123,6 +142,11 @@ function returnItemLine (data, numToAdd) {
 }
 
 function addItem (data) {
+    /**
+     * Returns:
+     * 0 to add new item to items
+     * >=1 to update existing item in items
+     */
     for (var i = 0; i < items.length; i++) {
         if (items[i].upc == data.upc) {
             // update item

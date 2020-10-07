770494af1990d18dc65919ce8ffd19dfc0e069d6
Xiao Owen Hu <owen.hu@mail.utoronto.ca>
2020-10-07 16:34:01 -0400

    Add item feature completed

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,42 +5,44 @@ var tax = 13; // default 13%
 const rowDelete = "<td class='text-right'><button type='button' class='close' aria-label='Close'><span aria-hidden='true'>&times;</span></button></td>";
 
 $('#addItem').click(function(){
-    var str = $('#item').val();
-    var num = items.length + 1;
-    var dict = {
-        name: str,
-        quantity: 1,
-        price: 10
-    };
-    items.push(dict);
+    var upc = $('#item').val();
 
     $.ajax({
         url: '/search',
         type: 'POST',
         cache: false,
-        data: { value: str },
+        data: { value: upc },
         dataType: 'json',
         success: function (data) {
-            alert(data.test);
+            onSuccess(data);
         },
         error: function (jqXHR, textStatus, err) {
             alert('text status ' + textStatus + ', err ' + err);
         }
     });
-
-    $('#myTable tr:last').after("<tr><td>"+str+"</td><td>1</td><td>10</td><td>10</td>"+rowDelete+"</tr>");
-    var sum = getSum();
-    $('#total').html("Discount -$"+ sum[0] +" &emsp; Tax $"+sum[1]+" &emsp; <b>Total $"+sum[2]+"</b>");
 });
 
 $('#myTable').on('click', 'button.close', function(e){
     e.preventDefault();
     var nRow = $(this).parents('tr')[0];
+    if (!deleteItem($(nRow).attr('id'))) {
+        alert("Could not find item with corresponding UPC. Please report to admin.");
+    };
     $(nRow).remove();
-    items.splice(nRow.rowIndex - 1, 1);
     var sum = getSum();
     $('#total').html("Discount -$"+ sum[0] +" &emsp; Tax $"+sum[1]+" &emsp; <b>Total $"+sum[2]+"</b>");
 });
+
+function deleteItem(id) {
+    var upc = parseInt(id.substring(1));
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].upc == upc) {
+            items.splice(i, 1);
+            return(1);
+        }
+    }
+    return(0);
+}
 
 function getSum() {
     var sum = 0;
@@ -76,3 +78,55 @@ $('#applyDiscount').click(function(){
     var sum = getSum();
     $('#total').html("Discount -$"+ sum[0] +" &emsp; Tax $"+sum[1]+" &emsp; <b>Total $"+sum[2]+"</b>");
 });
+
+function onSuccess (result) {
+    alert(result.name);
+
+    if (!result.found) {
+        alert("Item not found");
+        return;
+    }
+
+    var numToAdd = addItem(result);
+
+    if (numToAdd) {
+        // render updated row
+        $("#u"+ result.upc +"").html(returnItemLine(result, numToAdd));
+    } else {
+        // render new row
+        $('#myTable tr:last').after(returnItemLine(result, 0));
+    }
+
+    var sum = getSum();
+    $('#total').html("Discount -$"+ sum[0] +" &emsp; Tax $"+sum[1]+" &emsp; <b>Total $"+sum[2]+"</b>");
+}
+
+function returnItemLine (data, numToAdd) {
+    if (numToAdd) {
+        return("<td>"+data.upc+"</td><td>"+data.name+"</td><td>"+numToAdd+"</td><td>"+data.price+"</td><td>"
+            +(data.price * numToAdd).toFixed(2)+"</td>"+rowDelete);
+    } else {
+        return("<tr id='u"+data.upc+"'><td>"+data.upc+"</td><td>"+data.name+"</td><td>1</td><td>"+data.price+"</td><td>"
+            +data.price+"</td>"+rowDelete+"</tr>");
+    }
+}
+
+function addItem (data) {
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].upc == data.upc) {
+            // update item
+            alert("adding to existing");
+            items[i].quantity++;
+            return items[i].quantity;
+        }
+    }
+    // add item
+    alert("adding new");
+    items.push({
+        upc: data.upc,
+        name: data.name,
+        quantity: 1,
+        price: data.price
+    });
+    return 0;
+}
